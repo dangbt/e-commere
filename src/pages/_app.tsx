00edd58bd34af39e017fc118c8ab72cb056d72f2
Bùi Tấn Dâng <buitandang96@gmail.com>
2021-06-06 00:04:55 +0700

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'styles/globals.css';
+
+import type { AppProps /*, AppContext */ } from 'next/app';
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
